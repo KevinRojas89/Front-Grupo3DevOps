d264df8c67a57be70049a0c42c5112d77df16328
KevinRojas89 <kevin.rojas4268@unaula.edu.co>
2024-10-03 20:39:17 -0500

    Diseño del modulo principal

--- src/app/page.js
+++ src/app/page.js
@@ -1,97 +1,32 @@
-  import "./globals.css"
+ import "./globals.css"
 export default function Home() {
   return (
-    <div className="cuerpo">
-      <div className="header">
-        <div className="container">
+    <div className="bg-white font-sans ">
 
-          <img className="logo" src="/imagenes/logofront.svg"></img>
+      <div className="bg-white py-6">
+        <div className="container mx-auto flex items-center justify-between px-2">
+          <img className="h-32" src="/imagenes/logofront.svg"></img>
           <nav>
-            <ul className="links">
-              <li><a className="text" href="#">Quienes Somos?</a></li>
-              <li><a className="text" href="/src/app/postulate">Postulate</a></li>
+            <ul className="flex space-x-6 ">
+              <li><a className="text-black hover:text-blue-900" href="/src/app/QuienesSomos">Quienes Somos?</a></li>
+              <li><a className="text-black hover:text-blue-900" href="/src/app/postulate">Postulate</a></li>
             </ul>
           </nav>
-
         </div>
       </div>
 
-
-
-
-      <div className="hero">
-        <div className="fondoimagen">
-          <div className="text-content">
-
-          <h1 className="titulo1">Tecnología que <span>transforma</span></h1>
-          <h1 className="titulo1"><span>Soluciones</span> que perduran</h1>
-
-            <ul>
-              <li>Únete a <span>Esencia</span> y sé parte del futuro tecnológico.</li>
+      <div className="bg-white py-40  relative overflow-hidden">
+        <div className="container mx-auto px-0 relative z-10">
+          <div className="max-w-3xl">
+            <h1 className='text-5xl font-bold leading-tight mb-4'><span className="text-blue-800">Tecnología</span> que transforma<br></br><span className="text-blue-500">Soluciones</span> que perduran</h1>
+            <ul className='list-disc list-inside space-y-2 text-lg'>
+              <li>Únete a <span className="font-bold">Esencia</span> y sé parte del futuro tecnológico.</li>
               <li>Innovamos hoy, para cambiar el mañana.</li>
             </ul>
-
           </div>
-          <div className="image-content">
-            <img src="/imagenes/fondo1.png"></img>
-          </div>
-          
-        </div>
+        </div >
+        <img className="absolute top-36 right-10 w-1/2 transform translate-x-1/4 -translate-y-1/4" src="/imagenes/fondo1.png"></img>
       </div>
-
-      
-
-      
-
-
-
-
-
-
-
-      <div>
-        <p> lakalaklakalkalaklaklakalkalaklakalkalaklaklakalkalaklakalkalaklakalkalkalaklaklakalkalaklaklakalkalaklakalkalaklaklak
-          klal;akalkalkalaklakalkalaklakalkalaklaklakalkala
-        </p>
-
-      </div>
-
-
-
-      <div>
-
-
-
-
-      </div>
-
-
-
-      \
-
-      <div>
-        <h1>Postulate</h1>
-
-
-
-
-
-      </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     </div>
   );
 }
