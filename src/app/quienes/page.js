2a3744e72f2cdf55f5046761d16241998f75a32c
KevinRojas89 <kevin.rojas4268@unaula.edu.co>
2024-10-16 06:44:13 -0500

    Organizar Codigo

--- src/app/quienes/page.js
+++ src/app/quienes/page.js
@@ -1,5 +1,5 @@
 "use client";
-import "../globals.css";
+import "./quienes.css";
 
 const whoAreWe = () => {
   return (
@@ -36,57 +36,6 @@ const whoAreWe = () => {
         {/* Overlapping Circles */}
         <div className="circle circle-1"></div>
         <div className="circle circle-2"></div>
-
-        {/*se añaden y modifican estilos para el container*/}
-        <style jsx>{`
-          .container {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            padding: 20px;
-            position: relative;
-            max-width: 1200px;
-            margin: 50px auto;
-          }
-
-          .image-section {
-            flex: 1;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            position: relative;
-          }
-
-          .image-wrapper {
-            width: 400px;
-            height: 400px;
-            border-radius: 50%;
-            overflow: hidden;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-          }
-
-          .text-section {
-            flex: 1;
-            padding: 40px;
-            border-radius: 10px;
-            margin-left: -60px;
-            z-index: 1;
-          }
-
-          .description {
-            font-size: 18px;
-            line-height: 1.6;
-            color: #333;
-            max-width: 500px;
-          }
-
-          .text-section {
-            margin-left: 0;
-            text-align: center;
-          }
-        `}</style>
       </div>
       {/* se agrega la sección "container-2" con su respectiva animación showMe*/}
       <div className="container-2 showMe lg:flex lg:flex-row lg:space-x-56 lg:justify-center mt-52 pt-48 sm:pb-80 pb-48">
