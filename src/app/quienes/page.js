aa965a11377557bc2a3f7b9a9541df004765dbfb
Teo2214 <mateo.botero3812@unaula.edu.co>
2024-11-12 22:03:04 -0500

    correccion quienes somos css

--- src/app/quienes/page.js
+++ src/app/quienes/page.js
@@ -1,13 +1,12 @@
 "use client";
 import Image from "next/image";
-import "./quienes.css";
 import { useRouter } from "next/navigation";
 
 const WhoAreWe = () => {
   const router = useRouter();
-  
+
   const handleLogoClick = () => {
-    router.push("/"); 
+    router.push("/");
   };
 
   return (
@@ -15,11 +14,22 @@ const WhoAreWe = () => {
       {/* se agrega un panel principal que contiene todo "mainContainer" */}
       {/* se agrega mainTittle con animaciones alojadas en el "globals.css" aplicadas con tailwind */}
       <div className="mainTittle pb-80 pt-64 flex flex-col items-center">
-        <button data-tooltip-target="tooltip-home" type="button" class="inline-flex flex-col items-center justify-center px-5 rounded-tr-full rounded-tl-full hover:bg-gray-50 dark:hover:bg-gray-800 group">
-          <svg class="w-10 h-10 mb-1 text-gray-50 dark:text-gray-300 group-hover:text-blue-700 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20" onClick={handleLogoClick}>
+        <button
+          data-tooltip-target="tooltip-home"
+          type="button"
+          className="inline-flex flex-col items-center justify-center px-5 rounded-tr-full rounded-tl-full hover:bg-gray-50 dark:hover:bg-gray-800 group"
+        >
+          <svg
+            className="w-10 h-10 mb-1 text-gray-50 dark:text-gray-300 group-hover:text-blue-700 dark:group-hover:text-blue-500"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            onClick={handleLogoClick}
+          >
             <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
           </svg>
-          <span class="sr-only"></span>
+          <span className="sr-only"></span>
         </button>
         <Image
           src="/imagenes/logo.svg"
@@ -31,8 +41,8 @@ const WhoAreWe = () => {
       </div>
       {/* para las secciones de container y container-2, se les agrega la clase showMe para animación de scroll, 
       se recomienda agregar para cada sección nueva */}
-      <div className="container showMe bg-sky-500 rounded-md">
-        <div className="image-section slide-left">
+      <div className="container showMe bg-sky-500 rounded-md p-5 relative max-w-screen-xl mx-auto mt-12 mb-12">
+        <div className="image-section flex-1 flex justify-center items-center relative">
           <Image
             src="/imagenes/d.svg"
             alt="Technology Illustration"
@@ -41,8 +51,8 @@ const WhoAreWe = () => {
             height={493}
           />
         </div>
-        <div className="text-section bg-gradient-to-r from-sky-400 to-sky-200">
-          <p className="font-sans font-medium">
+        <div className="text-section flex-1 p-10 rounded-lg text-center bg-gradient-to-r from-sky-400 to-sky-200">
+          <p className="font-sans font-medium text-lg leading-7 text-gray-800 max-w-prose mx-auto">
             Esencia es una empresa de tecnología dedicada a desarrollar
             soluciones innovadoras que facilitan la transformación digital de
             empresas y organizaciones. Nos enfocamos en capturar la esencia de
@@ -55,7 +65,8 @@ const WhoAreWe = () => {
         <div className="circle circle-1"></div>
         <div className="circle circle-2"></div>
       </div>
-      {/* se agrega la sección "container-2" con su respectiva animación showMe*/}
+
+      {/* se agrega la sección "container-2" con su respectiva animación showMe */}
       <div className="container-2 showMe lg:flex lg:flex-row lg:space-x-56 lg:justify-center mt-52 pt-48 sm:pb-80 pb-48">
         <div className="valores">
           <div className="panel bg-sky-300 pb-32 w-64 relative rounded-md mb-32">
@@ -129,42 +140,44 @@ const WhoAreWe = () => {
       </div>
 
       <div className="bg-blue-950 text-white">
-        <div class="container mx-auto py-12">
-          <div class="sm:grid sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-8 block">
-            <div class="bg-blue-700 p-6">
-              <h3 class="font-bold text-lg">INNOVACIÓN</h3>
-              <p class="mt-2">
+        <div className="container mx-auto py-12">
+          <div className="sm:grid sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-8 block">
+            <div className="bg-blue-700 p-6">
+              <h3 className="font-bold text-lg">INNOVACIÓN</h3>
+              <p className="mt-2">
                 Nos enfocamos en crear soluciones que anticipen las necesidades
                 futuras.
               </p>
             </div>
-            <div class="bg-blue-500 p-6">
-              <h3 class="font-bold text-lg">CALIDAD</h3>
-              <p class="mt-2">
+            <div className="bg-blue-500 p-6">
+              <h3 className="font-bold text-lg">CALIDAD</h3>
+              <p className="mt-2">
                 Comprometidos con la entrega de productos y servicios de alta
                 calidad.
               </p>
             </div>
-            <div class="bg-blue-700 p-6">
-              <h3 class="font-bold text-lg">SOSTENIBILIDAD</h3>
-              <p class="mt-2">
+            <div className="bg-blue-700 p-6">
+              <h3 className="font-bold text-lg">SOSTENIBILIDAD</h3>
+              <p className="mt-2">
                 Desarrollamos tecnología respetuosa con el medio ambiente.
               </p>
             </div>
-            <div class="bg-blue-500 p-6">
-              <h3 class="font-bold text-lg">INTEGRIDAD</h3>
-              <p class="mt-2">Operamos bajo los más altos estándares éticos.</p>
+            <div className="bg-blue-500 p-6">
+              <h3 className="font-bold text-lg">INTEGRIDAD</h3>
+              <p className="mt-2">
+                Operamos bajo los más altos estándares éticos.
+              </p>
             </div>
-            <div class="col-span-2 sm:col-span-2 lg:col-span-1 bg-blue-600 p-6">
-              <h3 class="font-bold text-lg">COLABORACIÓN</h3>
-              <p class="mt-2">
+            <div className="col-span-2 sm:col-span-2 lg:col-span-1 bg-blue-600 p-6">
+              <h3 className="font-bold text-lg">COLABORACIÓN</h3>
+              <p className="mt-2">
                 Trabajamos de cerca con nuestros clientes para garantizar
                 soluciones que superen sus expectativas.
               </p>
             </div>
           </div>
-          <div class="lg:text-right text-center">
-            <h1 class="text-4xl font-bold">VALORES</h1>
+          <div className="lg:text-right text-center">
+            <h1 className="text-4xl font-bold">VALORES</h1>
           </div>
         </div>
       </div>
